7b12d20904ff1a36a4f6953bb79a836106002d6a
UniverseFinance <87629076+UniverseFinance@users.noreply.github.com>
2021-07-29 11:01:20 +0800

    Update SingleIntervalStrategy.sol
    rebalance

--- contracts/SingleIntervalStrategy.sol
+++ contracts/SingleIntervalStrategy.sol
@@ -457,17 +457,16 @@ contract SingleIntervalStrategy is IStrategy, Ownable {
         return _burnLiquidity(pool, liq, to);
     }
 
-    function reBalance() external override returns (uint256, uint256, int24, int24, bool) {
+    function reBalance() external override returns (uint256, uint256, int24, int24) {
         // Check status
         Config storage config = configs[msg.sender];
         // get Pool
         IUniswapV3Pool pool = IUniswapV3Pool(config.uniswapLp);
         // get New Ticks
         (bool status , int24 lowerTick, int24 upperTick) = _getReBalanceTicks(pool, config);
+        require(status, "reBalance status is False!");
         // get Position info
         Position storage position = positions[msg.sender];
-        // check rebalance status
-        if (!status || !position.isInit) {return (0, 0, lowerTick, upperTick, status);}
         // Burn All
         (uint256 feesFromPool0, uint256 feesFromPool1) = _burnAll(pool, position);
         // collect Fee
@@ -475,7 +474,7 @@ contract SingleIntervalStrategy is IStrategy, Ownable {
         // update position
         position.lowerTick = lowerTick;
         position.upperTick = upperTick;
-        return (feesFromPool0, feesFromPool1, lowerTick, upperTick, status);
+        return (feesFromPool0, feesFromPool1, lowerTick, upperTick);
     }
 
     function collectCommission(
